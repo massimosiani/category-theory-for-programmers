8ff43713ad592183e149cb6399cc513bfd5b76fe
Massimo Siani <maxsiani@gmail.com>
2018-11-22 10:15:04 +0100

    Add the contravariant

--- chapter08/Reader.hs
+++ chapter08/Reader.hs
@@ -1,7 +1,14 @@
 class Functor' f where
     fmap' :: (a -> b) -> f a -> f b
+class Contravariant' f where
+    fmap' :: (b -> a) -> f a -> f b
 
 type Reader r a = r -> a
 instance Functor' (Reader r) where
-    -- fmap :: (a -> b) -> (r -> a) -> (r -> b)
-    fmap f rx = f . rx -- \x -> f (rx x)
+    -- fmap' :: (a -> b) -> (r -> a) -> (r -> b)
+    fmap f rx = f . rx -- \x -> f (rx x), if \x has type r
+
+type Op r a = a -> r
+instance Contravariant' (Op r) where
+    -- fmap' :: (b -> a) -> (a -> r) -> (b -> r)
+    fmap' f ox = ox . f -- types align pretty well here
